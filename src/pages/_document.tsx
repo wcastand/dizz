22de91d476d0d4a2d7f24e33aa09a7470017b445
wcastand <wcastand@gmail.com>
2021-07-10 19:20:36 +0200

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,27 @@
 import withTwindDocument from '@twind/next/shim/document'
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+
 import twindConfig from '../twind.config'
 
-export default withTwindDocument(twindConfig)
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async defer data-domain='dizzapp.dev' src='https://mything.wcastand.dev/js/plausible.js'></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default withTwindDocument(twindConfig, MyDocument)
